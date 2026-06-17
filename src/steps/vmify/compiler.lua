baf35b9210be67e1f0abe95e75a78c980c84eb06
Casuke <kylellamas723@gmail.com>
2026-06-17 18:27:55 +0800

    Update compiler.lua

--- src/steps/vmify/compiler.lua
+++ src/steps/vmify/compiler.lua
@@ -12,7 +12,7 @@ function Compiler.new(blockMgr, funcScope, posVar)
   self.blockMgr  = blockMgr
   self.funcScope = funcScope
   self.posVar    = posVar
-  self.hoisted   = {}  -- list of hoisted local decls
+  self.hoisted   = {}
   return self
 end
 
@@ -68,24 +68,21 @@ function Compiler:addNilIfNeeded(block)
   end
 end
 
-function Compiler:compileBlock(stmtList, curBlock, exitBlock)
+function Compiler:compileBlock(stmtList, curBlock, exitBlock, loopContinueBlock)
   for _, stmt in ipairs(stmtList) do
-    curBlock = self:compileStmt(stmt, curBlock, exitBlock)
+    curBlock = self:compileStmt(stmt, curBlock, exitBlock, loopContinueBlock)
   end
   return curBlock
 end
 
-function Compiler:compileStmt(stmt, curBlock, exitBlock)
+function Compiler:compileStmt(stmt, curBlock, exitBlock, loopContinueBlock)
   local k  = stmt.kind
   local sc = curBlock.scope
 
-  -- Hoist local variable declarations to funcScope
   if k == "LocalVariableDeclaration" then
-    -- move variable ids to funcScope
     for _, id in ipairs(stmt.ids) do
       self.funcScope:addIfNotExists(id)
     end
-    -- if there are expressions, emit as assignment
     if stmt.expressions and #stmt.expressions > 0 then
       local lhs = {}
       for _, id in ipairs(stmt.ids) do
@@ -98,9 +95,7 @@ function Compiler:compileStmt(stmt, curBlock, exitBlock)
     return curBlock
 
   elseif k == "LocalFunctionDeclaration" then
-    -- hoist to funcScope too
     self.funcScope:addIfNotExists(stmt.id)
-    -- convert to function declaration
     local funcDecl = A.FunctionDeclaration(
       self.funcScope, stmt.id, {}, stmt.args, stmt.body
     )
@@ -122,7 +117,7 @@ function Compiler:compileStmt(stmt, curBlock, exitBlock)
 
       Block.addStmt(curBlock, self:taggedJmpCond(sc, cond, trueBlock.id, nextBlock.id))
 
-      local afterTrue = self:compileBlock(body.statements, trueBlock, finalBlock)
+      local afterTrue = self:compileBlock(body.statements, trueBlock, finalBlock, loopContinueBlock)
       self:addJumpIfNeeded(afterTrue, finalBlock.id)
 
       curBlock = nextBlock
@@ -137,14 +132,14 @@ function Compiler:compileStmt(stmt, curBlock, exitBlock)
           eifNext = finalBlock
         end
         Block.addStmt(curBlock, self:taggedJmpCond(sc, eif.condition, eifTrue.id, eifNext.id))
-        local afterEif = self:compileBlock(eif.body.statements, eifTrue, finalBlock)
+        local afterEif = self:compileBlock(eif.body.statements, eifTrue, finalBlock, loopContinueBlock)
         self:addJumpIfNeeded(afterEif, finalBlock.id)
         curBlock = eifNext
         sc = curBlock.scope
       end
 
       if elsebody then
-        local afterElse = self:compileBlock(elsebody.statements, curBlock, finalBlock)
+        local afterElse = self:compileBlock(elsebody.statements, curBlock, finalBlock, loopContinueBlock)
         self:addJumpIfNeeded(afterElse, finalBlock.id)
       else
         if curBlock ~= finalBlock then
@@ -164,7 +159,8 @@ function Compiler:compileStmt(stmt, curBlock, exitBlock)
     Block.addStmt(curBlock, self:taggedJmp(sc, checkBlock.id))
     Block.addStmt(checkBlock, self:taggedJmpCond(checkBlock.scope, stmt.condition, bodyBlock.id, finalBlock.id))
 
-    local afterBody = self:compileBlock(stmt.body.statements, bodyBlock, finalBlock)
+    -- pass checkBlock as loopContinueBlock so continue jumps back to condition check
+    local afterBody = self:compileBlock(stmt.body.statements, bodyBlock, finalBlock, checkBlock)
     self:addJumpIfNeeded(afterBody, checkBlock.id)
 
     return finalBlock
@@ -175,14 +171,23 @@ function Compiler:compileStmt(stmt, curBlock, exitBlock)
 
     Block.addStmt(curBlock, self:taggedJmp(sc, bodyBlock.id))
 
-    local afterBody = self:compileBlock(stmt.body.statements, bodyBlock, finalBlock)
-    if not blockHasJump(afterBody) then
-      Block.addStmt(afterBody, self:taggedJmpCond(afterBody.scope, stmt.condition, finalBlock.id, bodyBlock.id))
+    -- for repeat-until, continue jumps to the condition check at end of body
+    -- we use a dedicated continueTarget block placed before the condition
+    local continueTarget = self:newBlock()
+    local afterBody = self:compileBlock(stmt.body.statements, bodyBlock, finalBlock, continueTarget)
+
+    -- wire afterBody into continueTarget
+    self:addJumpIfNeeded(afterBody, continueTarget.id)
+
+    -- continueTarget evaluates the until condition
+    if not blockHasJump(continueTarget) then
+      Block.addStmt(continueTarget, self:taggedJmpCond(continueTarget.scope, stmt.condition, finalBlock.id, bodyBlock.id))
     end
 
     return finalBlock
 
   elseif k == "ForStatement" then
+    local checkBlock = self:newBlock()
     local finalBlock = self:newBlock()
     Block.addStmt(curBlock, stmt)
     self:addJumpIfNeeded(curBlock, finalBlock.id)
@@ -195,11 +200,20 @@ function Compiler:compileStmt(stmt, curBlock, exitBlock)
     return finalBlock
 
   elseif k == "DoStatement" then
-    return self:compileBlock(stmt.body.statements, curBlock, exitBlock)
+    return self:compileBlock(stmt.body.statements, curBlock, exitBlock, loopContinueBlock)
 
   elseif k == "BreakStatement" then
     if exitBlock then
       Block.addStmt(curBlock, self:taggedJmp(sc, exitBlock.id))
+    else
+      Block.addStmt(curBlock, self:taggedJmpNil(sc))
+    end
+    return self:newBlock()
+
+  elseif k == "ContinueStatement" then
+    -- jump to loop's continue target (top of while check, or continueTarget for repeat)
+    if loopContinueBlock then
+      Block.addStmt(curBlock, self:taggedJmp(sc, loopContinueBlock.id))
     else
       Block.addStmt(curBlock, self:taggedJmpNil(sc))
     end
